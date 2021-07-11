0a2c5e6f32a55ffcc072fa8ccba92c6751bff12e
Jocaagura <soporte@jocaagura.com>
2021-07-11 13:35:55 -0500

    Finalizacion de ejercicios de typescript

---
+++ angular/introtypescript/src/index.ts
@@ -0,0 +1,15 @@
+/*
+    ===== Código de TypeScript =====
+*/
+const timeStart: Date = new Date();
+
+
+
+
+
+
+const timeEnd: Date = new Date();
+console.log(
+  "Time elapsed",
+  timeEnd.getMilliseconds() - timeStart.getMilliseconds()
+);
